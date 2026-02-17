a0387f206ccdb820415ac6d06a0150eb14272b5f
Sooraj <sooraj.crafts@gmail.com>
2026-02-17 19:29:43 +0800

    Delete service.py

--- service.py
+++
@@ -1,9 +0,0 @@
-from humanoid.models import InitiateInteractionResponse, InteractionResponse
-
-
-def initiate_interaction() -> InitiateInteractionResponse:
-    return InitiateInteractionResponse(session_id="")
-
-
-def interact(session_id: str) -> InteractionResponse:
-    return InteractionResponse(next_interaction=session_id)
